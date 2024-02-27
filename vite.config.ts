1b76f6cd4ed8f672cdd817f4c334acc54d556512
Mario Orellana <ma_oa08@hotmail.com>
2024-02-27 12:36:09 -0600

    Implement basic chat widget as library

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,16 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    'process.env.NODE_ENV': JSON.stringify('production'),
+  },
   plugins: [react()],
+  build: {
+    lib: {
+      formats: ['umd'],
+      entry: 'src/App.tsx',
+      name: 'MedAssist',
+      fileName: (format) => `medassist.${format}.js`,
+    },
+  },
 });
