8e83d2ca03fde11bc8878b2a0e9bfef261730b44
Mario Orellana <ma_oa08@hotmail.com>
2024-03-11 17:53:57 -0600

    Implement VITE_AI_CONSULTATION_UUID env variable

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,6 @@ import App from './App.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App apiToken={import.meta.env.VITE_API_TOKEN} aiConsultationUuid='7d049feb-4ec3-469f-96b2-b24604559b05' />
+    <App apiToken={import.meta.env.VITE_API_TOKEN} aiConsultationUuid={import.meta.env.VITE_AI_CONSULTATION_UUID} />
   </React.StrictMode>,
 );
