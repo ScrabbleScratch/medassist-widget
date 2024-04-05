ca23b63c0c3933b9287cc14362c368f5212940d7
Mario Orellana <ma_oa08@hotmail.com>
2024-04-05 14:13:27 -0600

    Implement sendSintomatixEmail and sendSintomatixSms functions

--- src/utils/strapi/index.ts
+++ src/utils/strapi/index.ts
@@ -39,3 +39,47 @@ export const postAiAssistance = async (input: string, practitionerUuid: string, 
   
   return response.data;
 };
+
+// SYSTEM MESSAGES
+
+export async function sendSintomatixEmail(email: string, sintomatix: string, token: string): Promise<boolean> {
+  try {
+    const response = await strapi.post('/system_message_email', {
+      email,
+      type: 'sintomatix',
+      args: sintomatix,
+      channel: 'email',
+    }, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    return response.status === 200;
+  } catch (error) {
+    console.log(error);
+
+    return false;
+  }
+}
+
+export async function sendSintomatixSms(phone: string, sintomatix: string, token: string): Promise<boolean> {
+  try {
+    const response = await strapi.post('/system_message_email', {
+      phone,
+      type: 'sintomatix',
+      args: sintomatix,
+      channel: 'sms',
+    }, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    return response.status === 200;
+  } catch (error) {
+    console.log(error);
+
+    return false;
+  }
+}
